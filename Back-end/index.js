4d482b28b9f06eca6d273535b9632ab5997d3e1e
HuyBrox <128505633+HuyBrox@users.noreply.github.com>
2024-11-29 19:44:58 +0700

    Update index.js

--- Back-end/index.js
+++ Back-end/index.js
@@ -27,7 +27,8 @@ app.use(urlencoded({ extended: true }));
 
 // Cấu hình CORS
 const corsOptions = {
-    origin: [dotenv.config().parsed.PORT_CLIENT, "http://localhost:3000", "social-network-client-coral.vercel.app"],
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 };
 app.use(cors(corsOptions));
